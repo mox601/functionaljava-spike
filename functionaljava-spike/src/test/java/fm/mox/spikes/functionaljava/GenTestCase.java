ef4f05743b14cd1ec70e368665ce2b172c22c635
Matteo Moci <mmoci@expedia.com>
2017-04-10 10:28:09 +0200

    minor

--- functionaljava-spike/src/test/java/fm/mox/spikes/functionaljava/GenTestCase.java
+++ functionaljava-spike/src/test/java/fm/mox/spikes/functionaljava/GenTestCase.java
@@ -1,21 +1,5 @@
 package fm.mox.spikes.functionaljava;
 
-import fj.F;
-import fj.F2;
-import fj.P;
-import fj.P1;
-import fj.data.List;
-import fj.data.Option;
-import fj.data.Stream;
-import fj.test.CheckResult;
-import fj.test.Gen;
-import fj.test.Property;
-import fj.test.Rand;
-import fj.test.Shrink;
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.testng.annotations.Test;
-
 import static fj.P.p;
 import static fj.test.Arbitrary.arbCharacterBoundaries;
 import static fj.test.Arbitrary.arbList;
@@ -35,6 +19,21 @@ import static fm.mox.spikes.functionaljava.GenTestCase.Elevator.Floor.GROUND;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.Test;
+
+import fj.F;
+import fj.F2;
+import fj.P1;
+import fj.data.List;
+import fj.data.Option;
+import fj.test.CheckResult;
+import fj.test.Gen;
+import fj.test.Property;
+import fj.test.Rand;
+import fj.test.Shrink;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
  */
@@ -46,14 +45,14 @@ public class GenTestCase {
     static final Gen<String> ARB_STRING_BOUNDARIES = arbList(arbCharacterBoundaries).map(List::asString);
 
     static final Gen<User> ARB_USER = ARB_STRING_BOUNDARIES
-            .filter((String s) -> s.length() > 0 && s.length() < 10)
-            .bind(arbString.filter(p -> p.length() > 2 && p.length() < 10), username -> password -> new User(username, password));
+        .filter((String s) -> s.length() > 0 && s.length() < 10)
+        .bind(arbString.filter(p -> p.length() > 2 && p.length() < 10), username -> password -> new User(username, password));
 
 
     @Test
     public void givenGeneratedListAllItemsAreLtEq100() throws Exception {
         final F<List<Integer>, P1<Property>> allLtEq =
-                integers -> p(prop(integers.forall(integer -> integer <= 100)));
+            integers -> p(prop(integers.forall(integer -> integer <= 100)));
         final Property forall = forall(listOf(choose(0, 100)), shrinkList(shrinkInteger), allLtEq);
         assertTrue(forall.check(0, 10).isPassed());
     }
@@ -61,18 +60,18 @@ public class GenTestCase {
     @Test
     public void testFPScala() throws Exception {
         final F<List<Integer>, P1<Property>> reversedTwiceEqualsOriginal =
-                integers -> p(prop(integers.reverse().reverse().equals(integers)));
+            integers -> p(prop(integers.reverse().reverse().equals(integers)));
         final F<List<Integer>, P1<Property>> headEqualsLastOfReverse =
-                integers -> {
-                    boolean isHeadEqualsToLastOfReverse = true;
-                    final Option<Integer> headOpt = integers.headOption();
-                    if (headOpt.isSome()) {
-                        final Integer head = headOpt.some();
-                        final Integer lastOfReverse = integers.reverse().last();
-                        isHeadEqualsToLastOfReverse = head.equals(lastOfReverse);
-                    }
-                    return p(prop(isHeadEqualsToLastOfReverse));
-                };
+            integers -> {
+                boolean isHeadEqualsToLastOfReverse = true;
+                final Option<Integer> headOpt = integers.headOption();
+                if (headOpt.isSome()) {
+                    final Integer head = headOpt.some();
+                    final Integer lastOfReverse = integers.reverse().last();
+                    isHeadEqualsToLastOfReverse = head.equals(lastOfReverse);
+                }
+                return p(prop(isHeadEqualsToLastOfReverse));
+            };
         final Gen<Integer> choose = choose(0, 10);
         final Property reversedTwiceProp = forall(listOf(choose), shrinkList(shrinkInteger), reversedTwiceEqualsOriginal);
         final Property oppositeEqualsProp = forall(listOf(choose), shrinkList(shrinkInteger), headEqualsLastOfReverse);
