f83ceb82977a032fe73a2d43cf107ed8f6827c5d
Matteo Moci <mmoci@expedia.com>
2017-04-28 18:34:31 +0200

    new test

---
+++ functionaljava-spike/src/test/java/fm/mox/spikes/functionaljava/MonadExampleTestCase.java
@@ -0,0 +1,12 @@
+package fm.mox.spikes.functionaljava;
+
+import org.testng.annotations.Test;
+
+public class MonadExampleTestCase {
+
+    // http://blog.sigfpe.com/2006/08/you-could-have-invented-monads-and.html
+    @Test
+    public void test() throws Exception {
+
+    }
+}
