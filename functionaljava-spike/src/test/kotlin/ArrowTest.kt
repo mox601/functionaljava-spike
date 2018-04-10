e00b5f6d0da14615cd8177c3d337810da4fd58da
Matteo Moci <mox601@gmail.com>
2018-04-10 23:26:48 +0200

    validation test

--- functionaljava-spike/src/test/kotlin/ArrowTest.kt
+++ functionaljava-spike/src/test/kotlin/ArrowTest.kt
@@ -1,9 +1,12 @@
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
-import arrow.core.getOrElse
+import arrow.data.NonEmptyList
+import arrow.data.Validated
+import arrow.data.invalid
+import arrow.data.valid
 import org.testng.annotations.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  * Created by matteo (dot) moci (at) gmail (dot) com
@@ -11,19 +14,68 @@ import kotlin.test.assertEquals
 
 class ArrowTest {
 
-    fun double(x: Int): Int {
-        return 2 * x
-    }
-
     @Test
-    fun testDouble() {
-        val value1 = maybeItWillReturnSomething(true)
-        val value2 = maybeItWillReturnSomething(false)
-        assertEquals(4, double(2))
-        assertEquals("No value", value2.getOrElse { "No value" })
-        assertEquals("Found value", value1.getOrElse { "No value" })
+    fun testValidation() {
+        val config = Config(mapOf("url" to "127.0.0.1", "port" to "1337"))
+
+        val valid = parallelValidate(
+                config.parse(Read.stringRead, "url"),
+                config.parse(Read.intRead, "port")
+        ) { url, port ->
+            ConnectionParams(url, port)
+        }
+        assertTrue(valid.isValid)
     }
 
-    fun maybeItWillReturnSomething(flag: Boolean): Option<String> =
-            if (flag) Some("Found value") else None
+    data class ConnectionParams(val url: String, val port: Int)
+
+    fun <E, A, B, C> parallelValidate(v1: Validated<E, A>, v2: Validated<E, B>, f: (A, B) -> C): Validated<NonEmptyList<E>, C> {
+        return when {
+            v1 is Validated.Valid && v2 is Validated.Valid -> Validated.Valid(f(v1.a, v2.a))
+            v1 is Validated.Valid && v2 is Validated.Invalid -> v2.toValidatedNel()
+            v1 is Validated.Invalid && v2 is Validated.Valid -> v1.toValidatedNel()
+            v1 is Validated.Invalid && v2 is Validated.Invalid -> Validated.Invalid(NonEmptyList(v1.e, listOf(v2.e)))
+            else -> throw IllegalStateException("Not possible value")
+        }
+    }
+
+    sealed class ConfigError {
+        data class MissingConfig(val field: String) : ConfigError()
+        data class ParseConfig(val field: String) : ConfigError()
+    }
+
+    abstract class Read<A> {
+
+        abstract fun read(s: String): Option<A>
+
+        companion object {
+
+            val stringRead: Read<String> =
+                    object : Read<String>() {
+                        override fun read(s: String): Option<String> = Option(s)
+                    }
+
+            val intRead: Read<Int> =
+                    object : Read<Int>() {
+                        override fun read(s: String): Option<Int> =
+                                if (s.matches(Regex("-?[0-9]+"))) Option(s.toInt()) else None
+                    }
+        }
+    }
+
+    data class Config(val map: Map<String, String>) {
+        fun <A> parse(read: Read<A>, key: String): Validated<ConfigError, A> {
+            val v = Option.fromNullable(map[key])
+            return when (v) {
+                is Some -> {
+                    val s = read.read(v.t)
+                    when (s) {
+                        is Some -> s.t.valid()
+                        is None -> ConfigError.ParseConfig(key).invalid()
+                    }
+                }
+                is None -> Validated.Invalid(ConfigError.MissingConfig(key))
+            }
+        }
+    }
 }
