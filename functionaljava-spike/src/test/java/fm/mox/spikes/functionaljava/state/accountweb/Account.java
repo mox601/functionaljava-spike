e33d09e3d05a7d7b0fa0968dc2fa8bb676c40e43
Matteo Moci <mox601@gmail.com>
2018-04-24 00:18:26 +0200

    indent

--- functionaljava-spike/src/test/java/fm/mox/spikes/functionaljava/state/accountweb/Account.java
+++ functionaljava-spike/src/test/java/fm/mox/spikes/functionaljava/state/accountweb/Account.java
@@ -12,13 +12,19 @@ public class Account {
     public static StateMachine<Input, Outcome> createMachine() {
 
         Condition<Input, Outcome> canDeposit = stateTuple -> stateTuple.value.isDeposit();
-        Transition<Input, Outcome> deposit = stateTuple -> new Outcome(stateTuple.state.account + stateTuple.value.getAmount(), stateTuple.state.operations.cons(Either.right(stateTuple.value.getAmount())));
+        Transition<Input, Outcome> deposit = stateTuple -> new Outcome(
+                stateTuple.state.account + stateTuple.value.getAmount(),
+                stateTuple.state.operations.cons(Either.right(stateTuple.value.getAmount())));
 
         Condition<Input, Outcome> canWithdraw = stateTuple -> stateTuple.value.isWithdraw() && stateTuple.state.account >= stateTuple.value.getAmount();
-        Transition<Input, Outcome> withdraw = stateTuple -> new Outcome(stateTuple.state.account - stateTuple.value.getAmount(), stateTuple.state.operations.cons(Either.right(- stateTuple.value.getAmount())));
+        Transition<Input, Outcome> withdraw = stateTuple -> new Outcome(
+                stateTuple.state.account - stateTuple.value.getAmount(),
+                stateTuple.state.operations.cons(Either.right(- stateTuple.value.getAmount())));
 
         Condition<Input, Outcome> canFallback = stateTuple -> true;
-        Transition<Input, Outcome> fallback = stateTuple -> new Outcome(stateTuple.state.account, stateTuple.state.operations.cons(Either.left(new IllegalStateException(String.format("Can't withdraw %s because balance is only %s", stateTuple.value.getAmount(), stateTuple.state.account)))));
+        Transition<Input, Outcome> fallback = stateTuple -> new Outcome(
+                stateTuple.state.account,
+                stateTuple.state.operations.cons(Either.left(new IllegalStateException(String.format("Can't withdraw %s because balance is only %s", stateTuple.value.getAmount(), stateTuple.state.account)))));
 
         List<P2<Condition<Input, Outcome>, Transition<Input, Outcome>>> possibleTransitions = List.apply(
                 P.p(canDeposit, deposit),
